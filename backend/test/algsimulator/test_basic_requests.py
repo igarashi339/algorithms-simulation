9ba5f02f5285291da53b4b49c100a15c30685086
igarashi339 <igarashi339@gmail.com>
2021-02-20 17:41:01 +0900

    テストコードを修正

--- backend/test/algsimulator/test_basic_requests.py
+++ backend/test/algsimulator/test_basic_requests.py
@@ -4,6 +4,7 @@ import urllib.request
 BASE_URL = "http://localhost:8000"
 
 
-def test_sample():
+def test_ping():
     # APIサーバが起動していることを確認
-    req = urllib.request.Request(BASE_URL)
+    with urllib.request.urlopen(BASE_URL) as response:
+        assert response.getcode() == 200
