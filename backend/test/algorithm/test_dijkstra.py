6cb5a709718711d591cf3960d56dd0eb5432ce51
igarashi339 <igarashi339@gmail.com>
2021-02-26 21:52:24 +0900

    テストの期待値を修正

--- backend/test/algorithm/test_dijkstra.py
+++ backend/test/algorithm/test_dijkstra.py
@@ -6,7 +6,7 @@ from parameterized import parameterized
 
 class TestDijkstra(unittest.TestCase):
     def test_calc_shortest_path(self):
-        graph = Graph("5 -1 5 8 -1 -1 -1 -1 1 3 10 3 -1 -1 4 7 -1 4 -1 -1 5 -1 -1 -1 -1 -1")
+        graph = Graph("5 -1 5 8 -1 -1 -1 -1 1 3 10 3 -1 -1 1 7 -1 4 -1 -1 5 -1 -1 -1 -1 -1")
         assert graph.is_valid()
         dijkstra = Dijkstra(graph)
         start_node = 0
@@ -19,38 +19,38 @@ class TestDijkstra(unittest.TestCase):
     def get_expect_result1():
         cost_matrix = [-1, 5, 8, -1, -1,
                        -1, -1, 1, 3, 10,
-                       3, -1, -1, 4, 7,
+                       3, -1, -1, 1, 7,
                        -1, 4, -1, -1, 5,
                        -1, -1, -1, -1, -1]
         steps = list()
         steps.append(DijkstraOneStep(
             min_cost_node=0,
             cost_fixed_nodes=[0],
-            updated_labels={0: 0, 1: 5, 2: 8, 3: -1, 4: -1},
-            updated_prev_node={1: -1, 2: 0, 3: 0, 4: -1, 5: -1}
+            updated_labels=[0, 5, 8, -1, -1],
+            updated_prev_node={0: -1, 1: 0, 2: 0, 3: -1, 4: -1}
         ))
         steps.append(DijkstraOneStep(
             min_cost_node=1,
             cost_fixed_nodes=[0, 1],
-            updated_labels={0: 0, 1: 5, 2: 6, 3: 8, 4: 15},
-            updated_prev_node={1: -1, 2: 0, 3: 1, 4: 1, 5: 1}
+            updated_labels=[0, 5, 6, 8, 15],
+            updated_prev_node={0: -1, 1: 0, 2: 1, 3: 1, 4: 1}
         ))
         steps.append(DijkstraOneStep(
             min_cost_node=2,
             cost_fixed_nodes=[0, 1, 2],
-            updated_labels={0: 0, 1: 5, 2: 6, 3: 7, 4: 15},
-            updated_prev_node={1: -1, 2: 0, 3: 1, 4: 2, 5: 1}
+            updated_labels=[0, 5, 6, 7, 13],
+            updated_prev_node={0: -1, 1: 0, 2: 1, 3: 2, 4: 2}
         ))
         steps.append(DijkstraOneStep(
             min_cost_node=3,
             cost_fixed_nodes=[0, 1, 2, 3],
-            updated_labels={0: 0, 1: 5, 2: 6, 3: 7, 4: 12},
+            updated_labels=[0, 5, 6, 7, 12],
             updated_prev_node={0: -1, 1: 0, 2: 1, 3: 2, 4: 3}
         ))
         steps.append(DijkstraOneStep(
             min_cost_node=4,
             cost_fixed_nodes=[0, 1, 2, 3, 4],
-            updated_labels={0: 0, 1: 5, 2: 6, 3: 7, 4: 12},
+            updated_labels=[0, 5, 6, 7, 12],
             updated_prev_node={0: -1, 1: 0, 2: 1, 3: 2, 4: 3}
         ))
         return DijkstraSimulation(graph_size=5,
