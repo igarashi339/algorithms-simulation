a3c4835625291b6094bb438d51cab650ed7eac45
Nakajima2nd <gp38ate@gmail.com>
2021-03-01 22:41:29 +0900

    ネットワーク図サンプルを追加

--- frontend/react-app/src/index.js
+++ frontend/react-app/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Sample } from './Sample';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Sample />
   </React.StrictMode>,
   document.getElementById('root')
 );
