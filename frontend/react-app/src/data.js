64edaa602b7aa6e45bd250eda60d5aa969529961
Nakajima2nd <gp38ate@gmail.com>
2021-03-02 23:47:17 +0900

    ダイクストラ法のインプットフィールドを追加

---
+++ frontend/react-app/src/data.js
@@ -0,0 +1,17 @@
+export const dijkstraInput = [
+  {
+    key: 'startNode',
+    label: '開始ノード',
+    type: 'number'
+  },
+  {
+    key: 'goalNode',
+    label: '終了ノード',
+    type: 'number'
+  },
+  {
+    key: 'costMatrix',
+    label: 'コスト行列',
+    type: 'string'
+  }
+];
