eae7d19f8158587d9026a7a8ed4107dbe9dc6183
igarashi339 <igarashi339@gmail.com>
2021-03-16 09:56:47 +0900

    処理をメソッドに切り出し

--- frontend/react-app/src/dijkstra/dijkstra.js
+++ frontend/react-app/src/dijkstra/dijkstra.js
@@ -5,15 +5,60 @@ export const dijkstraParser = (response, setControl, setResult) => {
   if (response.data.status === 'NG') {
     setResult(response.data.error_message)
     setControl(3)
+    return;
   }
 
   if (response.data.status !== 'OK') {
     // todo: OKでもNGでもないためシステムエラー処理を追加する
+    return;
   }
 
-  const graph = getGraph(response)
+  const initialGraph = getInitialGraph(response)
 
-  // 初期テーブル
+  // 各ステップのグラフと表を作成
+  const steps = calcSteps(response, initialGraph)
+  const graphs = steps.map(step => step.graph);
+  const tables = steps.map(step => step.table);
+
+  // 開始ノード、終了ノード、最短経路を色付け
+  const startNode = response.data.search_info.start_node;
+  const goalNode = response.data.search_info.goal_node;
+  const shortestPath = response.data.search_info.shortest_path;
+  const coloredGraph = calcColoredGraph(initialGraph, graphs, startNode, goalNode, shortestPath)
+
+  setResult({ graphs: coloredGraph, tables, currentStep: 0 })
+  setControl(2)
+}
+
+const getInitialGraph = (response) => {
+  const graphSize = response.data.search_info.graph_size;
+  const costMatrix = response.data.search_info.cost_matrix;
+  const parsedMatrix = splitEvery(graphSize, costMatrix);
+  const nodes = range(0, graphSize).map(index => ({
+    id: index,
+    label: String(index)
+  }));
+  const edges = parsedMatrix.reduce((acc, cur, index) => {
+    cur.forEach((cost, cIndex) => {
+      if (cost > 0) {
+        acc.push({
+          from: index,
+          to: cIndex,
+          label: String(cost),
+          arrows: 'to'
+        })
+      }
+    })
+    return acc;
+  }, [])
+  return {
+    nodes: nodes,
+    edges: edges
+  };
+}
+
+const calcSteps = (response, graph) => {
+  // 初期テーブル作成
   const graphSize = response.data.search_info.graph_size;
   const initialTable = range(0, graphSize).map(index => ({
     id: index,
@@ -22,9 +67,7 @@ export const dijkstraParser = (response, setControl, setResult) => {
     prevNode: -1
   }));
 
-  // 各ステップのグラフと表を作成
-  const steps = response.data.search_info.steps.reduce((acc, cur) => {
-
+  return response.data.search_info.steps.reduce((acc, cur) => {
     // コスト最小のノードを更新
     const minCostNode = cur.min_cost_node;
     const last = acc.slice(-1)[0];
@@ -53,24 +96,24 @@ export const dijkstraParser = (response, setControl, setResult) => {
 
     return acc;
   }, [{ graph: graph, table: initialTable }])
+}
 
-  const graphs = steps.map(step => step.graph);
-  const tables = steps.map(step => step.table);
-
-  // 開始ノード、終了ノード、最短経路を色付け
-  const colordedEdges = graph.edges.map(edge => {
-    if (aperture(2, response.data.search_info.shortest_path).some(([from, to]) => (edge.from === from && edge.to === to))) {
+const calcColoredGraph = (initialGraph, graphs, startNode, goalNode, shortestPath) => {
+  const colordedEdges = initialGraph.edges.map(edge => {
+    if (aperture(2, shortestPath).some(([from, to]) => (edge.from === from && edge.to === to))) {
       return assoc('color', { color: 'red' }, edge)
     }
     else {
       return pipe(assoc('color', { opacity: '0.3' }), assoc('font', {color: 'lightgray'}))(edge)
     }
   })
+
   const coloredNodes = pipe(
-    update(response.data.search_info.start_node, assoc('color', 'red', graph.nodes[response.data.search_info.start_node])),
-    update(response.data.search_info.goal_node, assoc('color', 'red', graph.nodes[response.data.search_info.goal_node]))
-  )(graph.nodes.map(node => response.data.search_info.shortest_path.includes(node.id) ? assoc('color', 'salmon', node) : node));
-  const coloredGraph = update(
+    update(startNode, assoc('color', 'red', initialGraph.nodes[startNode])),
+    update(goalNode, assoc('color', 'red', initialGraph.nodes[goalNode]))
+  )(initialGraph.nodes.map(node => shortestPath.includes(node.id) ? assoc('color', 'salmon', node) : node));
+  
+  return update(
     graphs.length - 1,
     pipe(
       assoc('edges', colordedEdges),
@@ -78,36 +121,6 @@ export const dijkstraParser = (response, setControl, setResult) => {
     )(graphs.slice(-1)[0]),
     graphs
   );
-
-  setResult({ graphs: coloredGraph, tables, currentStep: 0 })
-  setControl(2)
-}
-
-const getGraph = (response) => {
-  const graphSize = response.data.search_info.graph_size;
-  const costMatrix = response.data.search_info.cost_matrix;
-  const parsedMatrix = splitEvery(graphSize, costMatrix);
-  const nodes = range(0, graphSize).map(index => ({
-    id: index,
-    label: String(index)
-  }));
-  const edges = parsedMatrix.reduce((acc, cur, index) => {
-    cur.forEach((cost, cIndex) => {
-      if (cost > 0) {
-        acc.push({
-          from: index,
-          to: cIndex,
-          label: String(cost),
-          arrows: 'to'
-        })
-      }
-    })
-    return acc;
-  }, [])
-  return {
-    nodes: nodes,
-    edges: edges
-  };
 }
 
 export const drawDijkstraGraph = (id, graphs, currentStep) => {
