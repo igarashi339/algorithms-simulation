1069da912ab315ef1935781b471adbe33c6e6afe
igarashi339 <igarashi339@gmail.com>
2021-03-17 10:58:54 +0900

    ダミーの入力を追加

--- frontend/react-app/src/dijkstra/dijkstra.test.js
+++ frontend/react-app/src/dijkstra/dijkstra.test.js
@@ -1,5 +1,105 @@
+// import { Rewire } from 'rewire'
+// var dijkstra = Rewire("./dijkstra.js")
+// var getInitialGraph = dijkstra.__get__("getInitialGraph")
 test(
-    "sample test", () => {
+    "getInitialTest", () => {
         expect(1 + 2).toBe(3);
     }
 );
+
+const getTargetResponse = () => {
+    return {
+        "status": "OK",
+        "search_info": {
+          "graph_size": 5,
+          "cost_matrix": [
+            -1, 5.0, 8.0, -1, -1,
+            -1, -1, 1.0, 3.0, 10.0,
+            3.0, -1, -1, 1.0, 7.0,
+            -1, 4.0, -1, -1, 5.0,
+            -1, -1, -1, -1, -1
+          ],
+          "start_node": 0,
+          "goal_node": 4,
+          "shortest_path": [
+            0, 1, 2, 3, 4
+          ],
+          "shortest_path_cost": 12.0,
+          "steps": [
+            {
+              "min_cost_node": 0,
+              "adjacent_nodes": [
+                1, 2
+              ],
+              "cost_fixed_nodes": [
+                0
+              ],
+              "updated_labels": [
+                0, 5.0, 8.0, -1, -1
+              ],
+              "updated_prev_node": [
+                -1, 0, 0, -1, -1
+              ]
+            },
+            {
+              "min_cost_node": 1,
+              "adjacent_nodes": [
+                2, 3, 4
+              ],
+              "cost_fixed_nodes": [
+                0, 1
+              ],
+              "updated_labels": [
+                0, 5.0, 6.0, 8.0, 15.0
+              ],
+              "updated_prev_node": [
+                -1, 0, 1, 1, 1
+              ]
+            },
+            {
+              "min_cost_node": 2,
+              "adjacent_nodes": [
+                0, 3, 4
+              ],
+              "cost_fixed_nodes": [
+                0, 1, 2
+              ],
+              "updated_labels": [
+                0, 5.0, 6.0, 7.0, 13.0
+              ],
+              "updated_prev_node": [
+                -1, 0, 1, 2, 2
+              ]
+            },
+            {
+              "min_cost_node": 3,
+              "adjacent_nodes": [
+                1, 4
+              ],
+              "cost_fixed_nodes": [
+                0, 1, 2, 3
+              ],
+              "updated_labels": [
+                0, 5.0, 6.0, 7.0, 12.0
+              ],
+              "updated_prev_node": [
+                -1, 0, 1, 2, 3
+              ]
+            },
+            {
+              "min_cost_node": 4,
+              "adjacent_nodes": [],
+              "cost_fixed_nodes": [
+                0, 1, 2, 3, 4
+              ],
+              "updated_labels": [
+                0, 5.0, 6.0, 7.0, 12.0
+              ],
+              "updated_prev_node": [
+                -1, 0, 1, 2, 3
+              ]
+            }
+          ]
+        }
+      };
+}
