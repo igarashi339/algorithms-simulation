706218b20953fd76d7139ab50f2a625ee0500b9e
Nakajima2nd <gp38ate@gmail.com>
2021-04-17 10:05:13 +0900

    404ページを整備

--- frontend/react-app/src/NotFound.jsx
+++ frontend/react-app/src/NotFound.jsx
@@ -1,7 +1,30 @@
-import { Typography } from '@material-ui/core';
+import { Box, makeStyles, Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { ErrorOutline } from '@material-ui/icons';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    // alignItems: 'center'
+  },
+  img: {
+    width: '30%',
+    height: 'auto'
+  },
+  link: {
+    paddingTop: '40px'
+  }
+}))
 
 export const NotFound = () => {
+  const classes = useStyles();
+
   return (
-    <Typography>Page Not Found</Typography>
+    <Box className={classes.root}>
+      <ErrorOutline className={classes.img} />
+      <Typography>Page Not Found</Typography>
+      <Link className={classes.link} to="/"><Typography>ホームに戻る</Typography></Link>
+    </Box>
   )
 }
